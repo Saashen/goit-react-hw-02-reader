e46f8cd05f33d0bfa7e80d2ca61143d8e3f2993f
Saashen <saashen.kyiv@gmail.com>
2019-12-23 14:43:16 +0200

    Updates

---
+++ precache-manifest.f316d14b545c841e748918a480fd564f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ee80d2c1218b88cbe6f26241444bff",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "253a95827183fd29c61f",
+    "url": "/goit-react-hw-02-reader/static/js/2.48f3df5f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-reader/static/js/2.48f3df5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ed869a57570b121fc70",
+    "url": "/goit-react-hw-02-reader/static/js/main.abc9e831.chunk.js"
+  },
+  {
+    "revision": "0bb802477d7166876060",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.5e4e2ba7.js"
+  }
+]);
